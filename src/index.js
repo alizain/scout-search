d220b92333a09c205f9ced809780af5839cbd1bf
Alizain Feerasta <alizain.feerasta@gmail.com>
2018-02-19 23:46:20 -0500

    Remove unneeded files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
 import collectiveSearch from './collectiveSearch'
 
 ReactDOM.render(<App searchEngine={collectiveSearch}/>, document.getElementById('root'))
-registerServiceWorker()
